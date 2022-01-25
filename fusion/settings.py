6be9e6a03e18cd48fbd227a80b18900a9de5bd66
gustaoliv <gusta.oliv4@gmail.com>
2022-01-25 11:04:04 -0300

    debug False

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l7i6&!du!7&rscl38d58qr*-s(%y6qoc*)i)!-zb6ek6am#a)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
